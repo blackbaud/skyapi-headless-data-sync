ed385877f04c6fee0593e4b0a245fb21f9c9b3c8
Michael Tims <michael.tims@blackbaud.com>
2020-05-04 09:00:27 -0400

    Retry request upon network exception

--- Services/DataSync/DataSyncService.Constituent.cs
+++ Services/DataSync/DataSyncService.Constituent.cs
@@ -2,20 +2,22 @@
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Net.Http;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace Blackbaud.HeadlessDataSync.Services.DataSync
 {
     public partial class DataSyncService
     {
-        private DateTimeOffset _lastConstituentSyncDate;
+        private DateTime _lastConstituentSyncDate;
 
         /// <summary>
         /// Syncs with Blackbaud constituent data.
         /// </summary>
         public async Task<bool> SyncConstituentDataAsync()
         {
-            _lastConstituentSyncDate = DateTimeOffset.Now;
+            _lastConstituentSyncDate = DateTime.Now;
             _dataStorageService.SetLastSyncDate(_lastConstituentSyncDate);
 
             var queryParams = _dataStorageService.GetConstituentQueryParams();
@@ -27,25 +29,41 @@ namespace Blackbaud.HeadlessDataSync.Services.DataSync
                 };
             }
 
-            var response = _constituentService.GetConstituents(queryParams);
-            if (response.IsSuccessStatusCode)
+            try
             {
-                var responseData = await response.Content.ReadAsStringAsync();
-                var json = JObject.Parse(responseData);
+                var response = _constituentService.GetConstituents(queryParams);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseData = await response.Content.ReadAsStringAsync();
+                    var json = JObject.Parse(responseData);
 
-                // Parse and store next_link params
-                Uri.TryCreate(json["next_link"].ToString(), UriKind.Absolute, out var nextLinkUri);
-                _dataStorageService.SetConstituentQueryParams(_constituentService.CreateQueryParamsFromNextLinkUri(nextLinkUri));
+                    // Parse and store next_link params
+                    Uri.TryCreate(json["next_link"].ToString(), UriKind.Absolute, out var nextLinkUri);
+                    _dataStorageService.SetConstituentQueryParams(_constituentService.CreateQueryParamsFromNextLinkUri(nextLinkUri));
 
-                // Update constituent data
-                UpdateConstituentData(json);
+                    // Update constituent data
+                    UpdateConstituentData(json);
 
-                return true;
+                    return true;
+                }
+
+                ShowError(response.StatusCode);
+
+                return false;
             }
+            catch (Exception ex)
+            {
+                Logger.LogInformation($"Exception: {ex.Message}.");
+                if (ex.InnerException is SocketException || ex.InnerException is HttpRequestException)
+                {
+                    // Exception occurred, continue to retry
+                    Logger.LogInformation("Retrying...");
+                    return true;
+                }
 
-            ShowError(response.StatusCode);
-
-            return false;
+                Logger.LogInformation("Exiting.");
+                return false;
+            }
         }
 
         /// <summary>
@@ -55,7 +73,7 @@ namespace Blackbaud.HeadlessDataSync.Services.DataSync
         {
             // Get count
             var count = int.Parse(json["count"].ToString());
-            Logger.LogInformation($"{count} constituents modified since {_lastConstituentSyncDate}");
+            Logger.LogInformation($"{_lastConstituentSyncDate}: {count} constituents modified since last sync");
 
             if (count > 0)
             {
